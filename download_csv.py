ffc47fab61b8959cf4cdceb85c84d2274bc6de64
mariaadradosr <mariaadradosr@gmail.com>
2021-03-30 16:17:04 +0200

    ccaa and provincias files added

--- download_csv.py
+++ download_csv.py
@@ -2,11 +2,11 @@ import requests
 
 url = 'https://cnecovid.isciii.es/covid19/resources/casos_hosp_uci_def_sexo_edad_provres.csv'
 
-def main(url):
+def main():
     print('downloading csv')
     req = requests.get(url)
     url_content = req.content
-    csv_file = open('casos_hosp_uci_def_sexo_edad_provres.csv', 'wb')
+    csv_file = open('input/casos_hosp_uci_def_sexo_edad_provres.csv', 'wb')
     csv_file.write(url_content)
     csv_file.close()
     print('csv saved')
